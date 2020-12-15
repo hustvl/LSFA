0017000c73d7eb000dcd5affc3cf5cb8a1e5e9cc
REXIMMORTAL <506458819@qq.com>
2020-12-15 11:37:12 +0800

    add external lib coviar

---
+++ external/pytorch-coviar/data/reencode_vid.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: ./reencode.sh [input dir] [output dir]"
+fi
+
+indir=$1
+outdir=$2
+
+mkdir outdir
+if [[ ! -d "${outdir}" ]]; then
+  echo "${outdir} doesn't exist. Creating it.";
+  mkdir -p ${outdir}
+fi
+
+for inname in $(ls ${indir}/*mp4)
+do
+	class_path="$(dirname "$inname")"
+	class_name="${class_path##*/}"
+
+	outname="${outdir}/${class_name}/${inname##*/}"
+	outname="${outname%.*}.mp4"
+
+	mkdir -p "$(dirname "$outname")"
+	ffmpeg -i ${inname} -vf setsar=1:1 -q:v 1 -c:v mpeg4 -f rawvideo ${outname}
+
+done
